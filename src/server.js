9ae3081751ea8eccebbeabe03cfa19dd8a526e07
JunJongHun <minn4072@gmail.com>
2022-03-11 01:50:09 +0900

    Create server.js

---
+++ src/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const PORT = 4000;
+
+const app = express();
+
+const handleHome = () => console.log("handleHome");
+
+app.get("/", handleHome);
+
+const handleListening = () => console.log(`sdaf: ${PORT}`);
+
+app.listen(PORT, handleListening);
