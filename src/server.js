07beb0573f06b984718ae50f22766f6e8a8cfedb
JunJongHun <minn4072@gmail.com>
2022-03-12 00:47:11 +0900

    Update server.js

--- src/server.js
+++ src/server.js
@@ -7,12 +7,16 @@ const app = express();
 const handleHome = (req, res) => {
   console.log(req);
   console.log("handleHome");
+  return res.end();
+};
+
+const handleLogin = (req, res) => {
+  return res.send("hi hi hi this is login part!");
 };
 
 app.get("/", handleHome);
+app.get("/login", handleLogin);
 
 const handleListening = () => console.log(`sdaf: ${PORT}`);
 
 app.listen(PORT, handleListening);
-
-print("노트북으로 시도");
