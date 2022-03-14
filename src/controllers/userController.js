4b7f7952d8e5d9e3c3b944abc56d41f1493052ce
JunJongHun <minn4072@gmail.com>
2022-03-15 00:52:10 +0900

    Create userController.js

---
+++ src/controllers/userController.js
@@ -0,0 +1,9 @@
+export const join = (req, res, next) => {
+  return res.send("user join");
+};
+export const edit = (req, res, next) => {
+  return res.send("user edit");
+};
+export const remove = (req, res, next) => {
+  return res.send("user remove");
+};
