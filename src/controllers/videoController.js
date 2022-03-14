e6666ea2dff1659a33889b74e0b83e3306d77cf7
JunJongHun <minn4072@gmail.com>
2022-03-15 00:52:12 +0900

    Create videoController.js

---
+++ src/controllers/videoController.js
@@ -0,0 +1,9 @@
+export const trending = (req, res, next) => {
+  return res.send("Home page videos");
+};
+export const watch = (req, res, next) => {
+  return res.send("video watch");
+};
+export const edit = (req, res, next) => {
+  return res.send("vidoe Edit");
+};
