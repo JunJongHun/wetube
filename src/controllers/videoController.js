d9d1293d9b96f6802020af578274ec5328efbc16
JunJongHun <minn4072@gmail.com>
2022-03-27 00:05:51 +0900

    Update videoController.js

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,11 +1,43 @@
+let videos = [
+  {
+    title: "Frist Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 1,
+  },
+
+  {
+    title: "Second Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 1,
+    id: 2,
+  },
+
+  {
+    title: "Third Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 3,
+  },
+];
+
 export const trending = (req, res, next) => {
-  return res.send("Home page videos");
+  return res.render("home", { pageTitle: "HOME", videos: videos });
 };
-export const see = (req, res, next) => {
-  return res.send("video See  ");
+export const watch = (req, res, next) => {
+  let id = req.params.id;
+  console.log(id);
+  const video = videos[id - 1];
+  return res.render("watch", { pageTitle: `Watch`, video: video });
 };
 export const edit = (req, res, next) => {
-  return res.send("vidoe Edit");
+  return res.render("edit");
 };
 export const search = (req, res, next) => {
   return res.send("user Search");
