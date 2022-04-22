8b81aa53532d7a05baf8eddf8d52e80fe6236547
JunJongHun <90402926+JunJongHun@users.noreply.github.com>
2022-04-23 01:36:45 +0900

    Update db.js

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 // const mongoose = require("mongoose");
 
 mongoose.connect(
-  "mongodb+srv://jonghun:0209@cluster0.wev7c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+  "주소입력"
 );
 
 const db = mongoose.connection;
