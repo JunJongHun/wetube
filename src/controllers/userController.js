4049c6d11689ee71d773af1bf567dd258ccaf367
JongHun <minn4072@gmail.com>
2022-05-23 02:17:29 +0900

    s

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/User";
+import bcrypt from "bcrypt";
 
 export const getJoin = (req, res, next) => {
   return res.render("join", { pageTitle: "Create Account" });
@@ -21,25 +22,53 @@ export const postJoin = async (req, res, next) => {
     });
   }
 
-  await User.create({
-    email,
-    username,
-    password,
-    name,
-    location,
-  });
+  try {
+    await User.create({
+      email,
+      username,
+      password,
+      name,
+      location,
+    });
+  } catch (error) {
+    return res.status(400).render("join", {
+      pageTitle: "Create Join",
+      errorMessage: error._message,
+    });
+  }
 
   return res.redirect("/login");
 };
 export const edit = (req, res, next) => {
   return res.send("user edit");
 };
+export const getLogin = (req, res, next) => {
+  return res.render("login", { pageTitle: "Log in" });
+};
+export const postLogin = async (req, res, next) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+  if (!user) {
+    return res.status(400).render("login", {
+      pageTitle: "Log in",
+      errorMessage: "Don't exists username.",
+    });
+  }
+  const passwordMatch = await bcrypt.compare(password, user.password);
+  if (!passwordMatch) {
+    return res.status(400).render("login", {
+      pageTitle: "Log in",
+      errorMessage: "Wrong Password!",
+    });
+  }
+  console.log("Success Login");
+  return res.redirect("/");
+};
+
 export const remove = (req, res, next) => {
   return res.send("user remove");
 };
-export const login = (req, res, next) => {
-  return res.send("user login");
-};
+
 export const see = (req, res, next) => {
   console.log(req);
   return res.send("user see ");
